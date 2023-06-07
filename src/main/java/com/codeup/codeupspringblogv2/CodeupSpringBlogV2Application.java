c14d3c7d457b15a5af845355bc8dfbe813955fc6
Savanna McCauley <savannamccauley@yahoo.com>
2023-06-07 15:09:10 -0500

    first

---
+++ src/main/java/com/codeup/codeupspringblogv2/CodeupSpringBlogV2Application.java
@@ -0,0 +1,13 @@
+package com.codeup.codeupspringblogv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeupSpringBlogV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeupSpringBlogV2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/codeup/codeupspringblogv2/CodeupSpringBlogV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeup.codeupspringblogv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeupSpringBlogV2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
